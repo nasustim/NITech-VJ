f2c2cf4e98541829bef116803a7b3f271081ce55
Mitsuhiro HIBINO <hibino17@iamas.ac.jp>
2017-11-19 16:13:04 +0900

    a

--- src/js/003.js
+++ src/js/003.js
@@ -16,18 +16,10 @@ var draw = () =>{
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
     let s = getSoundData();
-<<<<<<< HEAD
-
-    console.log(s[0]);
-
-    if(s[0] > 4){
-
-=======
 
     console.log(s[0]);
 
     if(s[0] > 0.01){
->>>>>>> 416ff20f99ed73b57f96f9f11e1ebbd3930e9648
         ctx.fillStyle = "rgb(255,255,255)";
         ctx.fillRect(20, 20, 80, 40);
     }
