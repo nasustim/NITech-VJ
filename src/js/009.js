7ef4923b229884b40ee28d9b9d8faee7f3b7fb60
Mitsuhiro HIBINO <hibino17@iamas.ac.jp>
2017-11-20 15:19:36 +0900

    aaa

---
+++ src/js/009.js
@@ -0,0 +1,212 @@
+var scene = null;
+var camera = null;
+var renderer = null;
+
+var radius = 500;
+var angle = 0;
+var plane = null;
+
+var scale = 1;
+
+var SIZE = 3;
+
+var geo;
+
+var wire, wireMesh;
+
+
+var xc = [];
+var yc = [];
+
+var count = 0;
+
+const MAX = 300;
+
+var cy = [];
+
+var step = [0,45,90,135];
+
+var xp = [];
+var yp = [];
+xp.push((Math.random()*MAX-MAX/2)); yp.push((Math.random()*MAX-MAX/2));
+xp.push((Math.random()*MAX-MAX/2)); yp.push((Math.random()*MAX-MAX/2));
+xp.push((Math.random()*MAX-MAX/2)); yp.push((Math.random()*MAX-MAX/2));
+
+var resize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth,window.innerHeight);
+}
+
+var init = () =>{
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(60,window.innerWidth/window.innerHeight,0.1,2000);
+    scene.add(camera);
+    camera.position.z = radius;
+
+    let light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(1000, 1000, 1000).normalize();
+    scene.add( light );
+
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window,innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    renderer.setClearColor(0x000000);
+
+    for(var i = 0; i< 8000 ;i+= 30){
+        xc.push(i * Math.cos(Math.PI/ 180 * i));
+        yc.push(i * Math.sin(Math.PI/ 180 * i));
+    }
+    geoInit();
+}
+
+var geoInit = () =>{
+    geo = new THREE.Geometry();
+};
+
+var geoUpdate = (wd) =>{
+
+    //count++;
+    //count = (count >= 360)? 0 : count;
+
+    for(let i = 0;i<4;i++){
+        step[i]++;
+        step[i] = (step[i] >= 360)? 0 : step[i];
+    }
+
+    geo = new THREE.Geometry();
+
+    for(let i = 0;i<4;i++) {
+        scene.remove(cy[i]);
+    }
+    //scene.remove(wireMesh);
+/*
+    for(let i = 0;i<xp.length;i++){
+        geo.vertices.push(new THREE.Vector3(xp[i], yp[i], 0));
+    }
+
+    for(let i = 0;i<xp.length;i+=3){
+        geo.faces.push(new THREE.Face3(i,i+1,i+2));
+    }
+
+    geo.computeFaceNormals();
+
+    let mat = new THREE.MeshBasicMaterial({color: 0x7777ff, wireframe: true});
+    plane = new THREE.Mesh(geo, mat);
+    scene.add(plane);
+*/
+
+    //let mat = new THREE.MeshBasicMaterial({color: 0x7777ff, wireframe: true});
+    //plane = new THREE.Mesh(geo, mat);
+    cy = [
+        new THREE.Mesh(
+            new THREE.CylinderBufferGeometry(20,20,5000,50),
+            new THREE.MeshPhongMaterial({
+                color: 0xffffff,
+                // ambient:0x777777,
+                specular:0xffffff,
+                shininess:200
+            })
+        ),
+        new THREE.Mesh(
+            new THREE.CylinderBufferGeometry(20,20,5000,50),
+            new THREE.MeshPhongMaterial({
+                color: 0xffffff,
+                // ambient:0x777777,
+                specular:0xffffff,
+                shininess:200
+            })
+        ),
+        new THREE.Mesh(
+            new THREE.CylinderBufferGeometry(20,20,5000,50),
+            new THREE.MeshPhongMaterial({
+                color: 0xffffff,
+                // ambient:0x777777,
+                specular:0xffffff,
+                shininess:200
+            })
+        ),
+        new THREE.Mesh(
+            new THREE.CylinderBufferGeometry(20,20,5000,50),
+            new THREE.MeshPhongMaterial({
+                color: 0xffffff,
+                // ambient:0x777777,
+                specular:0xffffff,
+                shininess:200
+            })
+        )
+    ];
+    for(let i = 0;i<4;i++) {
+        cy[i].position.set(100 * Math.sin(Math.PI / 180 * step[i]), 0, 0);
+        scene.add(cy[i]);
+    }
+
+    /*plane.geometry.verticesNeedUpdate = true;
+    for(let i = 0;i<plane.geometry.vertices.length;i++){
+        let vertex = plane.geometry.vertices[i];
+        let fl = (Math.floor(Math.random()*10)%2===0)?-1:1;
+        let fs = (Math.floor(Math.random()*10)%2===0)?-1:1;
+        // vertex.z = wd[i%wd.length]*scale*fl;
+        // vertex.y = wd[i%wd.length]*scale*fs;
+        vertex.z = wd[0]*scale*fl;
+        vertex.y = ((wd[1]+wd[0])/2)*scale*fs*xp[i];
+        //vertex.x = wd[i%wd.length]*1000000*fs;
+    }*/
+}
+
+var setup = () => {
+    init();
+    window.addEventListener("resize",resize,false);
+    draw();
+}
+
+var draw = () =>{
+    camera.position.x = 0;//radius*Math.sin(angle);
+    camera.position.y = 0;//radius*Math.sin(angle);
+    camera.position.z = 200;
+    //camera.position.x = 200;
+    //camera.position.y = 0;
+    //camera.position.z = 200;
+
+    camera.lookAt(new THREE.Vector3( 0, 0, 0 ));
+
+    geoUpdate(getSoundData());
+    //geoUpdate();
+
+    renderer.render(scene,camera);
+    window.requestAnimationFrame(draw);
+};
+
+
+
+var keyd = () =>{
+    console.log(`${event.key}`);
+
+    if(event.key=="1") {
+        //xp.push((Math.random()*MAX-MAX/2)); yp.push((Math.random()*MAX-MAX/2));
+        //xp.push((Math.random()*MAX-MAX/2)); yp.push((Math.random()*MAX-MAX/2));
+        //xp.push((Math.random()*MAX-MAX/2)); yp.push((Math.random()*MAX-MAX/2));
+        //SIZE+=3;
+
+        angle = ( 2 * Math.PI * parseInt(event.key) / 9) + angle;
+
+    }else if(event.key == "0"){
+
+        //xp=[(Math.random()*MAX-MAX/2)]; yp=[(Math.random()*MAX-MAX/2)];
+        //xp.push((Math.random()*MAX-MAX/2)); yp.push((Math.random()*MAX-MAX/2));
+        //xp.push((Math.random()*MAX-MAX/2)); yp.push((Math.random()*MAX-MAX/2));
+
+        //SIZE=3;
+    }
+
+    console.log(xp);
+
+    //if(event.key == "1"){
+//    angle = 2 * Math.PI * parseInt(event.key) / 9;
+    //}
+};
+
+document.onkeydown = keyd;
+
+setup();
