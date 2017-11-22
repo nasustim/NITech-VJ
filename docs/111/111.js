ade5dbea194af1fb48e9398b29428556fab4c03a
nasustim <mitsuhibino@gmail.com>
2017-11-22 20:18:41 +0900

    aa

---
+++ docs/111/111.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var setup = function setup() {
+    init();
+    window.addEventListener("resize", resize, false);
+    draw();
+};
+function draw() {
+    console.log(getSoundData()[0]);
+    window.requestAnimationFrame(draw);
+};
+
+var keyd = function keyd() {
+    console.log("" + event.key);
+
+    //if(event.key == "1"){
+    angle = 2 * Math.PI * parseInt(event.key) / 9;
+    //}
+};
+
+document.onkeydown = keyd;
+
+setup();
